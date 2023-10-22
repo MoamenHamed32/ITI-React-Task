34e7a24968a750ec21091e6dc774661868ce69da
Moamen Hamed <moamen.hamed33322@gmail.com>
2023-10-22 19:58:14 +0200

    using redux

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,26 +1,56 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../../Redux/counterSlice";
 import "./Counter.css";
+import { useState } from "react";
 
 export default function Counter() {
-  let [count, setCount] = useState(0);
-  const increaseCounter = () => {
-    setCount(++count);
+  const [inputValue, setInputValue] = useState(1);
+  const count = useSelector((state) => state.counter.counter);
+  const dispatch = useDispatch();
+  const handleIncrement = (value) => {
+    dispatch(increment(value));
   };
-  const decreaseCount = () => {
-    if (count > 0) {
-      setCount(--count);
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  const handleInputChange = (e) => {
+    if (e.target.value > 0) {
+      setInputValue(e.target.value);
     }
   };
+
   return (
     <section id="counter">
-      <div className="container d-flex justify-content-center align-items-center ">
-        <button className="btn btn-danger " onClick={decreaseCount}>
-          -
-        </button>
-        <p className="mx-3 fs-4 my-0">{count}</p>
-        <button className="btn btn-success" onClick={increaseCounter}>
-          +
-        </button>
+      <div className="container">
+        <div className="counter-btns d-flex justify-content-center align-items-center h-50">
+          <button className="btn btn-danger " onClick={handleDecrement}>
+            -
+          </button>
+          <p className="mx-3 fs-4 my-0">{count}</p>
+          <button
+            className="btn btn-success"
+            onClick={() => handleIncrement(parseInt(inputValue))}
+          >
+            +
+          </button>
+        </div>
+        <div className="add-by-amount d-flex justify-content-center align-items-center">
+          <h3>Increase By :</h3>
+          <input
+            onChange={handleInputChange}
+            className="amount-value"
+            type="text"
+            placeholder="value"
+            value={inputValue}
+          />
+          <button
+            className="btn btn-success"
+            onClick={() => handleIncrement(parseInt(inputValue))}
+          >
+            Add
+          </button>
+        </div>
       </div>
     </section>
   );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import "./Home.css";
 export default function Home() {
+  const count = useSelector((state) => state.counter.counter);
   return (
     <section
       id="home"
       className="d-flex justify-content-center align-items-center"
     >
-      <h1>Home Section</h1>
+      <h1>Counter is : {count}</h1>
     </section>
   );
 }
